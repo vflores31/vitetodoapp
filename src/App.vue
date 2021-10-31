07cd5cabb30e2b8ad438b692c511983ef5df5942
Victoria <victoriaflowers13@yahoo.com>
2021-10-31 09:38:45 -0500

    Finished App

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,15 @@
 <script setup>
-import { ref, computed } from "vue";
-
-const newTodo = ref("");
-
-const todos = ref([]);
-
-const pending = computed(() => {
-  return todos.value.filter((todo) => !todo.done);
-});
-
-const completed = computed(() => {
-  return todos.value.filter((todo) => todo.done);
-});
-
-const addTodo = () => {
-  if (newTodo.value.trim()) {
-    todos.value.push({
-      id: todos.value.length,
-      content: newTodo.value,
-      done: false,
-    });
-    newTodo.value = "";
-  }
-};
-
-const changeStatus = (id) => {
-  const todo = todos.value.find((todo) => todo.id === id);
-  todo.done = !todo.done;
-};
+import Title from "./components/Title.vue";
+import List from "./components/List.vue";
 </script>
 
 <template>
   <div class="min-h-screen bg-purple-800">
     <div class="container flex flex-col pt-8 mx-auto space-y-8">
-      <h1
-        class="pb-4 text-6xl font-thin tracking-tight text-center text-gray-300"
-      >
-        🚀 My To Do App
-      </h1>
-      <input
-        @change="addTodo"
-        v-model="newTodo"
-        type="text"
-        class="px-4 py-2 text-xl text-center rounded-lg"
-        placeholder="New Todo"
-      />
+      <Title />
       <div class="flex justify-around">
-        <div class="w-1/3">
-          <h3 class="text-2xl text-center text-blue-400">Pending</h3>
-          <ul class="pt-8 space-y-4">
-            <li
-              v-for="todo in pending"
-              :key="todo.id"
-              @click="changeStatus(todo.id)"
-              class="w-full px-4 py-2 font-bold text-center text-blue-600 transition-colors duration-500 bg-gray-300 rounded-lg  hover:cursor-pointer hover:bg-blue-400 hover:text-gray-200"
-            >
-              {{ todo.content }}
-            </li>
-          </ul>
-        </div>
-        <div class="w-1/3">
-          <h3 class="text-2xl text-center text-green-400">Completed</h3>
-          <ul class="pt-8 space-y-4">
-            <li
-              v-for="todo in completed"
-              :key="todo.id"
-              @click="changeStatus(todo.id)"
-              class="w-full px-4 py-2 font-bold text-center text-green-600 transition-colors duration-500 bg-gray-300 rounded-lg  hover:cursor-pointer hover:bg-green-400 hover:text-gray-200"
-            >
-              {{ todo.content }}
-            </li>
-          </ul>
-        </div>
+        <List />
+        <List isCompleted />
       </div>
     </div>
   </div>
---
+++ src/components/Title.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from "vue";
+import useTodos from "../composables/useTodos";
+
+const { addTodo } = useTodos();
+
+const newTodo = ref("");
+const add = () => {
+  if (newTodo.value) {
+    addTodo(newTodo.value);
+    newTodo.value = "";
+  }
+};
+</script>
+
+<template>
+  <h1 class="pb-4 text-6xl font-thin tracking-tight text-center text-gray-300">
+    🚀 My To Do App
+  </h1>
+  <input
+    @change="add"
+    v-model="newTodo"
+    type="text"
+    class="px-4 py-2 text-xl text-center rounded-lg"
+    placeholder="New Todo"
+  />
+</template>
